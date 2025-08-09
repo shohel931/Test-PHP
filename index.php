322a588dce14756223af063481a7d02b46e17889
MD. Shohel Rana <shohel065931@gmail.com>
2025-08-09 18:14:03 +0600

    g

--- index.php
+++ index.php
@@ -16,8 +16,8 @@
     <div class="nav_area">
         <ul>
             <li><a href="index.php"><i class="fa-solid fa-house"></i></a></li>
-            <li><a href="about.php">About</a></li>
-            <li><a href="contact.php">Contact</a></li>
+            <li><a href="videos.php"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m380-340 280-180-280-180v360Zm-60 220v-80H160q-33 0-56.5-23.5T80-280v-480q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v480q0 33-23.5 56.5T800-200H640v80H320ZM160-280h640v-480H160v480Zm0 0v-480 480Z"/></svg></a></li>
+            <li><a href="settings.php">Contact</a></li>
         </ul>
     </div>
     <div class="login_area">
