b086d609c2ebf53e57578cce57d736a98099f922
MD. Shohel Rana <shohel065931@gmail.com>
2025-08-09 09:48:02 +0600

    gg

--- login.php
+++ login.php
@@ -1,5 +1,13 @@
 <?php 
+include 'confige.php'; 
+session_start();
 
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    
+}
 
 
 
