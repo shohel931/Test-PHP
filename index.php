a99368a90db00d849a1e064d534c1779eeac9480
MD. Shohel Rana <shohel065931@gmail.com>
2025-08-09 09:59:40 +0600

    gh

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="login_register.css">
+    <title>My Site</title>
+</head>
+<body>
+
+<div class="home_page">
+<h1>Welcome to My Site</h1>
+<a href="login.php">Login Here</a>
+<a href="register.php">Register</a>
+</div>
+</body>
+</html>
--- login.php
+++ login.php
@@ -6,7 +6,19 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $username = $_POST['username'];
     $password = $_POST['password'];
 
-    
+    // username validation
+   $sql = "SELECT * FROM users WHERE username = ?";
+   $stmt = $conn->prepare($sql);
+   $stmt->bind_param("s", $username);
+   $stmt->execute();
+   $result = $stmt->get_result();
+
+    if ($result->num_rows === 1) {
+        $user = $result->fetch_assoc();
+        .
+    }
+
+
 }
 
 
