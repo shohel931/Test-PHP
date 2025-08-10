2318460c1b815555156b29636683da99977bf1ce
MD. Shohel Rana <shohel065931@gmail.com>
2025-08-10 06:28:45 +0600

    h

--- videos.php
+++ videos.php
@@ -27,9 +27,9 @@ if (!isset($_SESSION['user_id'])) {
     </div>
     <div class="nav_area">
         <ul>
-            <li><a href="index.php"><i class="fa-solid fa-house"></i></a></li>
+            <li><a href="index.php"></a></li>
             <li><a href="videos.php"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000"><path d="m380-340 280-180-280-180v360Zm-60 220v-80H160q-33 0-56.5-23.5T80-280v-480q0-33 23.5-56.5T160-840h640q33 0 56.5 23.5T880-760v480q0 33-23.5 56.5T800-200H640v80H320ZM160-280h640v-480H160v480Zm0 0v-480 480Z"/></svg></a></li>
-            <li><a href="settings.php">Contact</a></li>
+            <li><a href="settings.php"></a></li>
         </ul>
     </div>
     <div class="login_area">
@@ -63,7 +63,7 @@ if (!isset($_SESSION['user_id'])) {
             <div class="sub_contain_area">
 
             <div class="create_post">
-                
+
                 <div class="profile_area">
                     <a href="#"><img src="./img/log.png" alt=""></a>
                     <div class="name_time">
@@ -74,10 +74,6 @@ if (!isset($_SESSION['user_id'])) {
 
                 <div class="post_text">
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet non vero sapiente illum iure repellat praesentium ea reiciendis, libero dignissimos.</p>
-                </div>
-
-                <div class="post_image">
-                    <img src="./img/post.jpg" alt="Post Image">
                 </div>
 
                 <div class="post_video">
