20aef540568aa3894dc9ea937eb84e83fe293281
MD. Shohel Rana <shohel065931@gmail.com>
2025-08-10 15:54:16 +0600

    f

--- friends.php
+++ friends.php
@@ -19,7 +19,7 @@ if (!isset($_SESSION['user_id'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/home_page.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css">
-    <title>Home</title>
+    <title>Friends</title>
 </head>
 <body>
 
